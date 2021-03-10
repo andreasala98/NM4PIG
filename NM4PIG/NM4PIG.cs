68bf9b932e4454457ca7b945f95309f593f9e6e4
Andrea Sala <andrea.sala98@gmail.com>
2021-03-10 16:16:41 +0100

    Minor updates

--- NM4PIG/NM4PIG.cs
+++ NM4PIG/NM4PIG.cs
@@ -9,19 +9,6 @@ namespace NM4PIG
         {
             Console.WriteLine("Hello World! I love colors");
 
-            Color col1 = new Color(4.0,5.0,6.0);
-            Color col2 = new Color(1.0, 2.0, 3.0);
-            Color sum=col1+col2, diff=col1-col2;
-            Console.WriteLine("sum=("+sum.r+","+sum.g+","+sum.b+")");
-            Console.WriteLine("diff=("+diff.r+","+diff.g+","+diff.b+")");
-            Color purple = new Color(1.3, 4.65, 5.22);
-
-            Console.WriteLine("Green component: " + purple.g);
-
-          
-
-            Console.WriteLine("Green product component: " + purple.g);
-
         }
     }
 }
