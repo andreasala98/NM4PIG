20e5d6f410983ec384ee98993e410641cebdca44
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-03-17 11:29:41 +0100

    HdrImages errori da sistemare

--- Trace/HdrImage.cs
+++ Trace/HdrImage.cs
@@ -1,5 +1,5 @@
 using System;
-using Xunit;
+
 
 namespace Trace
 {
@@ -19,38 +19,55 @@ namespace Trace
             this.height = y;
             
             // SE NECESSARIO, INIZIALIZZA TUTTI GLI ELEMENTI DEL VETTORE Color pixel A 0.
-            /*for (int nrow = 0, y-1, nrow++)
+            this.pixel = new Color[x * y];
+            for (int nrow = 0; nrow < y; nrow++)
             {
-                for (int ncol = 0, x-1, ncol++)
+                for (int ncol = 0; ncol < x; ncol++)
                 {
-                    pixel[nrow * width + ncol] = new Color(0.0, 0.0, 0.0);
+                    this.pixel [nrow * width + ncol] = new Color(0.0, 0.0, 0.0);
                 }
-            }*/
+            }
         }
 
         // CONTROLLA CHE LE COORDINATE x, y SIANO ALL'INTERNO DELL'IMMAGINE.
-        public bool validCoords(int x, int y, this)
+        public bool validCoords(int x, int y)
         {
-            return (x >= 0) & (x <= this.width) & (y >= 0) & (y <= this.height);
+            return (x >= 0) & (x < this.width) & (y >= 0) & (y < this.height);
      
         }
 
         // RESTITUISCE L'INDICE DEL VETTORE pixel CHE RAPPRESENTA IL PUNTO CON COORDINATE (y, x) [-> riga = y, colonna = x]
-        public int pixelOffset(int x, int y, this)
+        public int pixelOffset(int x, int y)
         {
             return (y * this.width + x);
         }
+
+
         public Color getPixel(int x, int y)
         {
-            Assert.True(validCoords(x, y));
-            return pixel[pixelOffset(x,y)];
+            if (validCoords(x, y))
+            {
+                return pixel[pixelOffset(x,y)];
+            }
+            else 
+            {
+                //Color a = new Color(0.0, 0.0, 0.0);
+                Console.WriteLine("Pixel fuori dall'immagine; errore!");
+                return new Color(0,0,0);
+            }
         }
+
 
         public void setPixel(int x, int y, Color a)
         {
-            Assert.True(validCoords(x,y));
-            pixel[pixelOffset(x,y)] = a;
-
+            if (validCoords(x,y))
+            {
+                pixel[pixelOffset(x,y)] = a;
+            }
+            else
+            {
+                Console.WriteLine("Pixel fuori dall'immagine; errore!");
+            }
         }
 
     }
