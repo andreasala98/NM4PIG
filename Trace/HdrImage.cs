667c97ae0b2e517745c25f7d2f56604f4adaa170
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-03-17 11:03:29 +0100

    added HdrImage function validCoords & pixelOffset

--- Trace/HdrImage.cs
+++ Trace/HdrImage.cs
@@ -1,4 +1,5 @@
 using System;
+using Xunit;
 
 namespace Trace
 {
@@ -11,11 +12,11 @@ namespace Trace
         public int height;
         public Color[] pixel;
 
-        public HdrImage(int x, int y,)
+        public HdrImage(int x, int y)
         {
             // x NUMBER OF COLUMNS, y NUMBER OF ROWS
-            width = x;
-            height = y;
+            this.width = x;
+            this.height = y;
             
             // SE NECESSARIO, INIZIALIZZA TUTTI GLI ELEMENTI DEL VETTORE Color pixel A 0.
             /*for (int nrow = 0, y-1, nrow++)
@@ -27,18 +28,32 @@ namespace Trace
             }*/
         }
 
+        // CONTROLLA CHE LE COORDINATE x, y SIANO ALL'INTERNO DELL'IMMAGINE.
+        public bool validCoords(int x, int y, this)
+        {
+            return (x >= 0) & (x <= this.width) & (y >= 0) & (y <= this.height);
+     
+        }
+
+        // RESTITUISCE L'INDICE DEL VETTORE pixel CHE RAPPRESENTA IL PUNTO CON COORDINATE (y, x) [-> riga = y, colonna = x]
+        public int pixelOffset(int x, int y, this)
+        {
+            return (y * this.width + x);
+        }
         public Color getPixel(int x, int y)
         {
-            assert validCoords(x, y);
+            Assert.True(validCoords(x, y));
             return pixel[pixelOffset(x,y)];
         }
 
         public void setPixel(int x, int y, Color a)
         {
-            assert validCoords(x,y);
+            Assert.True(validCoords(x,y));
             pixel[pixelOffset(x,y)] = a;
 
         }
 
     }
+
+    
 }
