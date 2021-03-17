9284748c3f6d8960fba12fe1788ac364eaab1ce1
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-03-17 10:42:08 +0100

    HdrImage constructor added

--- Trace/HdrImage.cs
+++ Trace/HdrImage.cs
@@ -4,11 +4,28 @@ namespace Trace
 {
     // CLASSE HdrImage
 
+
     public struct HdrImage
     {
         public int width;
         public int height;
         public Color[] pixel;
+
+        public HdrImage(int x, int y,)
+        {
+            // x NUMBER OF COLUMNS, y NUMBER OF ROWS
+            width = x;
+            height = y;
+            
+            // SE NECESSARIO, INIZIALIZZA TUTTI GLI ELEMENTI DEL VETTORE Color pixel A 0.
+            /*for (int nrow = 0, y-1, nrow++)
+            {
+                for (int ncol = 0, x-1, ncol++)
+                {
+                    pixel[nrow * width + ncol] = new Color(0.0, 0.0, 0.0);
+                }
+            }*/
+        }
 
         public Color getPixel(int x, int y)
         {
