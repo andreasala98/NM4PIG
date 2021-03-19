483661141053460699f43ee7243030ffca3df099
Andrea Sala <andrea.sala98@gmail.com>
2021-03-19 10:43:16 +0100

    * HdrTest.cs: Minor changes
    * NM4PIG.cs: Just trying to use VS2019

--- NM4PIG/NM4PIG.cs
+++ NM4PIG/NM4PIG.cs
@@ -8,16 +8,23 @@ namespace NM4PIG
     {
         static void Main(string[] args)
         {
-            var MyImg = new HdrImage(3, 2);
+            var MyImg = new HdrImage(1024, 1024);
 
-            string fileName = "blackFile.pfm";
+            for (int i=1; i<=1024; i++){
+                for (int j=1; j<=1024; j++){
+                    if((i+j)%2==0) MyImg.setPixel(i-1,j-1, new Color(100f,100f,100f));
+                    else MyImg.setPixel(i-1,j-1, new Color(0f,0f,100f));
+                }
+            }
+            
+            string fileName = "chessBoard.pfm";
 
             using (FileStream fileStream = File.OpenWrite(fileName))
             {
                 MyImg.savePfm(fileStream);
             }
 
-                Console.WriteLine($"Saved {fileName}");
+                Console.WriteLine($"{fileName} correctly saved!");
         }
     }
 }
