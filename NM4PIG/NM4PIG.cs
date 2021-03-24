b744a57c2273ad6d252dfbf0a5ec0f1d7ce232ba
Andrea Sala <andrea.sala98@gmail.com>
2021-03-24 10:40:14 +0100

    readPfm implemented.

--- NM4PIG/NM4PIG.cs
+++ NM4PIG/NM4PIG.cs
@@ -8,18 +8,10 @@ namespace NM4PIG
     {
         static void Main(string[] args)
         {
-            var MyImg = new HdrImage(1024, 1024);
+            var MyImg = new HdrImage(4, 4);
 
-            for (int i = 1; i <= 1024; i++)
-            {
-                for (int j = 1; j <= 1024; j++)
-                {
-                    if ((i + j) % 2 == 0) MyImg.setPixel(i - 1, j - 1, new Color(100f, 100f, 100f));
-                    else MyImg.setPixel(i - 1, j - 1, new Color(0f, 0f, 100f));
-                }
-            }
-
-            string fileName = "chessBoard.pfm";
+           
+            string fileName = "blackFile.pfm";
 
             using (FileStream fileStream = File.OpenWrite(fileName))
             {
