88ef4f121677235374a66458f387612e2e0279a2
Tommaso Armadillo <tommasoarmadillo@MacBook-Pro-di-Tommaso.local>
2021-03-24 10:39:45 +0100

    Example how to throw exceptions and catch them

--- NM4PIG/NM4PIG.cs
+++ NM4PIG/NM4PIG.cs
@@ -8,25 +8,9 @@ namespace NM4PIG
     {
         static void Main(string[] args)
         {
-            var MyImg = new HdrImage(1024, 1024);
+            Console.WriteLine("Hello, world!");
 
-            for (int i = 1; i <= 1024; i++)
-            {
-                for (int j = 1; j <= 1024; j++)
-                {
-                    if ((i + j) % 2 == 0) MyImg.setPixel(i - 1, j - 1, new Color(100f, 100f, 100f));
-                    else MyImg.setPixel(i - 1, j - 1, new Color(0f, 0f, 100f));
-                }
-            }
 
-            string fileName = "chessBoard.pfm";
-
-            using (FileStream fileStream = File.OpenWrite(fileName))
-            {
-                MyImg.savePfm(fileStream);
-            }
-
-            Console.WriteLine($"{fileName} correctly saved!");
         }
     }
-}
+
