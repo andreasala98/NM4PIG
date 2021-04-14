de38623e2c9a5df0bfb328f32fad5f9443481e38
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-04-14 11:32:45 +0200

    Definitions of Point struct without tests

--- Trace/Geometry.cs
+++ Trace/Geometry.cs
@@ -14,6 +14,34 @@ namespace Trace
             this.y = y;
             this.z = z;
         }
+
+        // convert to string
+        public override string ToString() => $"Vec(x={this.x}, y={this.y}, z={this.z})";
+
+        // isClose method to Tests purposes
+        public static bool isClose(float a, float b)
+        {
+            var epsilon = 1e-8F;
+            return Math.Abs(a - b) < epsilon;
+        }
+
+        public bool isClose(Point A)
+        {
+            return isClose(this.x, A.x) && isClose(this.y, A.y) && isClose(this.z, A.z);
+        }
+
+        // sum between Point and Vector, which gives back a Point
+        public static Point operator +(Point p, Vec v) 
+            => new Point(p.x + v.x, p.y + v.y, p.z + v.z);
+
+        // difference between Point and Vector, which gives back a Point
+        public static Point operator +(Point p, Vec v) 
+            => new Point(p.x - v.x, p.y - v.y, p.z - v.z);
+            
+        // difference between two Points, which gives back a Vector
+        public static Point operator -(Point p, Point v) 
+            => new Vec(p.x - v.x, p.y - v.y, p.z - v.z);
+
     }
 
     public struct Vec
