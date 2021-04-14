7c3770e95b74d632e4ec2aa6d043fc384961aee8
Andrea Sala <andrea.sala98@gmail.com>
2021-04-14 11:32:45 +0200

    Added some vector operations

--- Trace/Geometry.cs
+++ Trace/Geometry.cs
@@ -28,6 +28,31 @@ namespace Trace
             this.y = y;
             this.z = z;
         }
+
+        public static Vec operator *(float alfa, Vec a)
+            => new Vec (a.x * alfa, a.y * alfa, a.z * alfa);
+
+        public static Vec operator /(float alfa, Vec a)
+            => new Vec (a.x / alfa, a.y / alfa, a.z / alfa);
+
+        public static float operator *(Vec a, Vec b)
+            => a.x * b.x + a.y * b.y + a.z * b.z;
+
+        public static Vec crossProd (Vec a, Vec b)
+           => new Vec (x = a.y * b.z - a.z * b.y, 
+                       y = a.z * b.x - a.x * b.z,
+                       z = a.x * b.y - a.y * b.x);
+
+        public float getSquaredNorm ()
+            => this * this;
+
+        public float getNorm ()
+            => sqrt(this.getSquaredNorm());
+
+        public Vec Normalize()
+            => this/norm;
+        
+
     }
 
 }
