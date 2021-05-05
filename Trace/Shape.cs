a56aa6bae0742eaf438f33ea650623b603cd3d86
Tommaso Armadillo <tommaso.armadillo@gmail.com>
2021-05-05 11:26:28 +0200

    Ended implementations of shapes and spheres

--- Trace/Shape.cs
+++ Trace/Shape.cs
@@ -1,23 +1,132 @@
+/*
+The MIT License (MIT)
+
+Copyright © 2021 Tommaso Armadillo, Pietro Klausner, Andrea Sala
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+the Software. THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+*/
+
 using System;
 
 namespace Trace
 {
+    /// <summary>
+    /// Abstract class that implements a generic shape
+    /// </summary>
     public abstract class Shape
     {
+        /// <summary>
+        /// <see cref="Transformation associated to the shape">
+        /// </summary>
         public Transformation transformation;
 
+        /// <summary>
+        /// Create a unit sphere, potentially associating a transformation to it
+        /// </summary>
+        /// <param name="transformation"><see cref="Transformation"> associated to the sphere</param>
         public Shape(Transformation? transformation = null)
         {
             this.transformation = transformation ?? new Transformation(1);
         }
 
-        public abstract HitRecord? RayIntersection(Ray ray)
+        /// <summary>
+        /// Compute the intersection between a ray and this shape. This method has to be ridefined in
+        /// derived classes
+        /// </summary>
+        public abstract HitRecord? RayIntersection(Ray ray);
 
     }
 
+    /// <summary>
+    /// A 3D unit sphere centered on the origin of the axes
+    /// </summary>
     public class Sphere : Shape
     {
 
+        /// <summary>
+        /// Create a unit sphere, potentially associating a transformation to it
+        /// </summary>
+        /// <param name="transformation"><see cref="Transformation"> associated to the sphere</param>
         public Sphere(Transformation? transformation = null) : base(transformation) { }
+
+        /// <summary>
+        /// Checks if a ray intersects the sphere
+        /// </summary>
+        /// <param name="ray"><see cref="Ray"> that you want to check if intersect the sphere</param>
+        /// <returns><see cref="HitRecord"> or <see cref="null"> if no intersection was found.</returns>
+        public override HitRecord? RayIntersection(Ray ray)
+        {
+            Ray invRay = ray.Transform(this.transformation.getInverse());
+            Vec originVec = invRay.origin.toVec();
+            float a = invRay.dir.getSquaredNorm();
+            float b = 2.0f * originVec * invRay.dir;
+            float c = originVec.getSquaredNorm() - 1.0f;
+
+            float delta = b * b - 4.0f * a * c;
+            if (delta <= 0.0f)
+                return null;
+
+            float sqrtDelta = (float)Math.Sqrt((float)delta);
+            float tmin = (-b - sqrtDelta) / (2.0f * a);
+            float tmax = (-b + sqrtDelta) / (2.0f * a);
+
+            float firstHitT;
+            if (tmin > invRay.tmin && tmin < invRay.tmax)
+                firstHitT = tmin;
+            else if (tmax > invRay.tmin && tmax < invRay.tmax)
+                firstHitT = tmax;
+            else
+                return null;
+
+            Point hitPoint = invRay.at(firstHitT);
+            return new HitRecord(
+                worldPoint: this.transformation * hitPoint,
+                normal: this.transformation * _sphereNormal(hitPoint, ray.dir),
+                surfacePoint: _spherePointToUV(hitPoint),
+                t: firstHitT,
+                ray: ray
+            );
+        }
+
+        /// <summary>
+        /// Compute the normal of a unit sphere<br/>
+        /// The normal is computed for <see cref="Point"> (a point on the surface of the
+        /// sphere), and it is chosen so that it is always in the opposite
+        /// direction with respect to `ray_dir`.
+        /// </summary>
+        /// <param name="point"><see cref="Point"> on the surface of the sphere</param>
+        /// <param name="rayDir"><see cref="Vec"> radius of the sphere</param>
+        /// <returns><see cref="Normal"> object</returns>
+        private static Normal _sphereNormal(Point point, Vec rayDir)
+        {
+            Normal result = new Normal(point.x, point.y, point.z);
+            if (point.toVec() * rayDir < 0.0f)
+                result = -result;
+            return result;
+        }
+
+        /// <summary>
+        /// Convert a 3D point on the surface of the unit sphere into a (u, v) 2D point
+        /// </summary>
+        /// <param name="point">3D <see cref="Point"/></param>
+        /// <returns><see cref="Vec2D"/></returns>
+        private static Vec2D _spherePointToUV(Point point)
+            => new Vec2D(
+                    u: (float)Math.Atan2(point.y, point.x) / (2.0f * Constant.PI),
+                    v: Math.Acos(point.z) / Constant.PI
+                );
+
+
+
     }
 }
