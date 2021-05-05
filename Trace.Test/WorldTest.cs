90e73d9af76828fa53f5cf8f2c7fe194b98a9f7c
Tommaso Armadillo <tommaso.armadillo@gmail.com>
2021-05-05 15:32:23 +0200

    One file left in merge

--- Trace.Test/WorldTest.cs
+++ Trace.Test/WorldTest.cs
@@ -25,7 +25,6 @@ namespace Trace.Test
     public class HitRecordAndWorldTest
     {
         [Fact]
-<<<<<<< HEAD
         public void TestIsCloseHitRecord()
         {
             HitRecord myHR = new HitRecord(new Point(0.0f, 0.0f, 1.0f), new Normal(3.0f, 5.0f, 6.0f), new Vec2D(-3.0f, 7.0f), 3.5f, new Ray(new Point(10.0f, 4.0f, 4.0f), Constant.VEC_X));
@@ -45,31 +44,4 @@ namespace Trace.Test
         }
     } //end of class
 
-=======
-        public void TestIsCloseHitRecord() 
-        {
-            HitRecord myHR = new HitRecord(new Point(0.0f, 0.0f, 1.0f), new Normal(3.0f,5.0f,6.0f), new Vec2D(-3.0f,7.0f), 3.5f, new Ray(new Point(10.0f,4.0f,4.0f), Constant.VEC_X));
-            HitRecord myHR2 = new HitRecord(new Point(0.0f, 0.0f, 1.0f), new Normal(3.0f,5.0f,6.0f), new Vec2D(-3.0f,7.0f), 3.5f, new Ray(new Point(10.0f,4.0f,4.0f), Constant.VEC_X));
-            HitRecord myHR3 = new HitRecord(new Point(0.0f, 0.0f, 2.0f), new Normal(-1.0f,5.0f,6.0f), new Vec2D(-3.0f,7.0f), 3.5f, new Ray(new Point(0.0f,4.0f,4.0f), Constant.VEC_X));
-
-
-            Assert.True(myHR.isClose(myHR2));
-            Assert.False(myHR.isClose(myHR3));
-            return;
-        }
-
-        [Fact]
-        public void TestRayIntersection() 
-        {
-            World w = new World();
-            // mettere 2 sfere e verifiare che l'HitRecord è quello della sfera più avanti
-            w.addShape(new Sphere());
-        }
-
-
-
-
-    } //end of class
-
->>>>>>> Working on world test
 } //end of namespace
