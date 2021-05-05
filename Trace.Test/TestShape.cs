54760dcd69b2974840df7b9ab609c16e87381999
Tommaso Armadillo <tommaso.armadillo@gmail.com>
2021-05-05 12:40:13 +0200

    Implemented passing test for Sphere

--- Trace.Test/TestShape.cs
+++ Trace.Test/TestShape.cs
@@ -44,8 +44,8 @@ namespace Trace.Test
             HitRecord? intersection2 = sphere.rayIntersection(ray2);
             Assert.True(intersection2 != null, "TestHit failed! - Assert 3/5");
             HitRecord hit2 = new HitRecord(
-                new Point(1.0f, 0.0f, 1.0f),
-                new Normal(1.0f, 0.0f, 1.0f),
+                new Point(1.0f, 0.0f, 0.0f),
+                new Normal(1.0f, 0.0f, 0.0f),
                 new Vec2D(0.0f, 0.5f),
                 2.0f,
                 ray2
@@ -53,82 +53,61 @@ namespace Trace.Test
             Assert.True(hit2.isClose(intersection2), "TestHit failed! - Assert 4/5");
 
             Assert.True(sphere.rayIntersection(new Ray(new Point(0f, 10f, 2f), -Constant.VEC_Z)) == null, "TestHit failed! - Assert 5/5 ");
+        }
 
+        [Fact]
+        public void TestInnerHit()
+        {
+            Sphere sphere = new Sphere();
+            Ray ray = new Ray(origin: new Point(0f, 0f, 0f), dir: Constant.VEC_X);
+            HitRecord? intersection = sphere.rayIntersection(ray);
+            Assert.True(intersection != null, "TestInnerHit failed! - Assert 1/2");
+
+            HitRecord hit = new HitRecord(
+                                            new Point(1.0f, 0.0f, 0.0f),
+                                            new Normal(-1.0f, 0.0f, 0.0f),
+                                            new Vec2D(0.0f, 0.5f),
+                                            1.0f,
+                                            ray
+                                        );
+            Assert.True(hit.isClose(intersection), "TestInnerHit failed! - Assert 2/2");
+        }
+
+        [Fact]
+        public void TestTransformation()
+        {
+            Sphere sphere = new Sphere(Transformation.Translation(new Vec(10.0f, 0.0f, 0.0f)));
+
+            Ray ray1 = new Ray(new Point(10f, 0f, 2f), -Constant.VEC_Z);
+            HitRecord? intersection1 = sphere.rayIntersection(ray1);
+            Assert.True(intersection1 != null, "TestTransformation failed - assert 1/6");
+            HitRecord hit1 = new HitRecord(
+                                            new Point(10.0f, 0.0f, 1.0f),
+                                            new Normal(0.0f, 0.0f, 1.0f),
+                                            new Vec2D(0.0f, 0.0f),
+                                            1.0f,
+                                            ray1
+                                        );
+            Assert.True(hit1.isClose(intersection1), "TestTransformation failed - assert 2/6");
+
+            Ray ray2 = new Ray(new Point(13f, 0f, 0f), -Constant.VEC_X);
+            HitRecord? intersection2 = sphere.rayIntersection(ray2);
+            Assert.True(intersection2 != null, "TestTransformation failed - assert 3/6");
+            HitRecord hit2 = new HitRecord(
+                                            new Point(11.0f, 0.0f, 0.0f),
+                                            new Normal(1.0f, 0.0f, 0.0f),
+                                            new Vec2D(0.0f, 0.5f),
+                                            2.0f,
+                                            ray2
+                                        );
+            Assert.True(hit2.isClose(intersection2), "TestTransformation failed - assert 4/6");
+
+            // Check if the sphere failed to move by trying to hit the untransformed shape
+            Assert.True(sphere.rayIntersection(new Ray(new Point(0f, 0f, 2f), -Constant.VEC_Z)) == null, "TestTransformation failed - assert 5/6");
+
+            // Check if the *inverse* transformation was wrongly applied
+            Assert.True(sphere.rayIntersection(new Ray(new Point(-10f, 0f, 0f), -Constant.VEC_Z)) == null, "TestTransformation failed - assert 6/6");
         }
     }
 
 }
-
-/*class TestSphere(unittest.TestCase):
-    def testHit(self):
-        sphere = Sphere()
-
-        ray1 = Ray(origin = Point(0, 0, 2), dir = -VEC_Z)
-        intersection1 = sphere.ray_intersection(ray1)
-        assert intersection1
-        assert HitRecord(
-            world_point=Point(0.0, 0.0, 1.0),
-            normal = Normal(0.0, 0.0, 1.0),
-            surface_point = Vec2d(0.0, 0.0),
-            t = 1.0,
-            ray = ray1,
-        ).is_close(intersection1)
-
-        ray2 = Ray(origin = Point(3, 0, 0), dir = -VEC_X)
-        intersection2 = sphere.ray_intersection(ray2)
-        assert intersection2
-        assert HitRecord(
-            world_point=Point(1.0, 0.0, 0.0),
-            normal = Normal(1.0, 0.0, 0.0),
-            surface_point = Vec2d(0.0, 0.5),
-            t = 2.0,
-            ray = ray2,
-        ).is_close(intersection2)
-
-        assert not sphere.ray_intersection(Ray(origin=Point(0, 10, 2), dir = -VEC_Z))
-
-    def testInnerHit(self):
-        sphere = Sphere()
-
-        ray = Ray(origin = Point(0, 0, 0), dir = VEC_X)
-        intersection = sphere.ray_intersection(ray)
-        assert intersection
-        assert HitRecord(
-            world_point=Point(1.0, 0.0, 0.0),
-            normal = Normal(-1.0, 0.0, 0.0),
-            surface_point = Vec2d(0.0, 0.5),
-            t = 1.0,
-            ray = ray,
-        ).is_close(intersection)
-
-    def testTransformation(self):
-        sphere = Sphere(transformation = translation(Vec(10.0, 0.0, 0.0)))
-
-        ray1 = Ray(origin = Point(10, 0, 2), dir = -VEC_Z)
-        intersection1 = sphere.ray_intersection(ray1)
-        assert intersection1
-        assert HitRecord(
-            world_point=Point(10.0, 0.0, 1.0),
-            normal = Normal(0.0, 0.0, 1.0),
-            surface_point = Vec2d(0.0, 0.0),
-            t = 1.0,
-            ray = ray1,
-        ).is_close(intersection1)
-
-        ray2 = Ray(origin = Point(13, 0, 0), dir = -VEC_X)
-        intersection2 = sphere.ray_intersection(ray2)
-        assert intersection2
-        assert HitRecord(
-            world_point=Point(11.0, 0.0, 0.0),
-            normal = Normal(1.0, 0.0, 0.0),
-            surface_point = Vec2d(0.0, 0.5),
-            t = 2.0,
-            ray = ray2,
-        ).is_close(intersection2)
-
-        # Check if the sphere failed to move by trying to hit the untransformed shape
-assert not sphere.ray_intersection(Ray(origin=Point(0, 0, 2), dir = -VEC_Z))
-
-        # Check if the *inverse* transformation was wrongly applied
-        assert not sphere.ray_intersection(Ray(origin=Point(-10, 0, 0), dir = -VEC_Z))
-*/
