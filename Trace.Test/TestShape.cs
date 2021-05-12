76bb4b2d298e2729dfd0f0b91bb673680e108bc1
Andrea Sala <andrea.sala98@gmail.com>
2021-05-12 10:44:48 +0200

    Implemented Plane class with tests

--- Trace.Test/TestShape.cs
+++ Trace.Test/TestShape.cs
@@ -17,6 +17,7 @@ IN THE SOFTWARE.
 */
 
 using Xunit;
+using System;
 
 namespace Trace.Test
 {
@@ -24,13 +25,13 @@ namespace Trace.Test
     {
 
         [Fact]
-        public void TestHit()
+        public void TestHitSphere()
         {
             Sphere sphere = new Sphere();
 
             Ray ray1 = new Ray(origin: new Point(0f, 0f, 2f), dir: -Constant.VEC_Z);
             HitRecord? intersection1 = sphere.rayIntersection(ray1);
-            Assert.True(intersection1 != null, "TestHit failed! - Assert 1/5");
+            Assert.True(intersection1 != null, "TestHitSphere failed! - Assert 1/5");
             HitRecord hit1 = new HitRecord(
                 new Point(0.0f, 0.0f, 1.0f),
                 new Normal(0.0f, 0.0f, 1.0f),
@@ -38,11 +39,11 @@ namespace Trace.Test
                 1.0f,
                 ray1
             );
-            Assert.True(hit1.isClose(intersection1), "TestHit failed! - Assert 2/5");
+            Assert.True(hit1.isClose(intersection1), "TestHitSphere failed! - Assert 2/5");
 
             Ray ray2 = new Ray(new Point(3f, 0f, 0f), -Constant.VEC_X);
             HitRecord? intersection2 = sphere.rayIntersection(ray2);
-            Assert.True(intersection2 != null, "TestHit failed! - Assert 3/5");
+            Assert.True(intersection2 != null, "TestHitSphere failed! - Assert 3/5");
             HitRecord hit2 = new HitRecord(
                 new Point(1.0f, 0.0f, 0.0f),
                 new Normal(1.0f, 0.0f, 0.0f),
@@ -50,13 +51,13 @@ namespace Trace.Test
                 2.0f,
                 ray2
             );
-            Assert.True(hit2.isClose(intersection2), "TestHit failed! - Assert 4/5");
+            Assert.True(hit2.isClose(intersection2), "TestHitSphere failed! - Assert 4/5");
 
-            Assert.True(sphere.rayIntersection(new Ray(new Point(0f, 10f, 2f), -Constant.VEC_Z)) == null, "TestHit failed! - Assert 5/5 ");
+            Assert.True(sphere.rayIntersection(new Ray(new Point(0f, 10f, 2f), -Constant.VEC_Z)) == null, "TestHitSphere failed! - Assert 5/5 ");
         }
 
         [Fact]
-        public void TestInnerHit()
+        public void TestInnerHitSphere()
         {
             Sphere sphere = new Sphere();
             Ray ray = new Ray(origin: new Point(0f, 0f, 0f), dir: Constant.VEC_X);
@@ -109,4 +110,33 @@ namespace Trace.Test
             Assert.True(sphere.rayIntersection(new Ray(new Point(-10f, 0f, 0f), -Constant.VEC_Z)) == null, "TestTransformation failed - assert 6/6");
         }
     }
+
+
+    public class TestPlane
+    {
+
+        [Fact]
+        public void TestHitPlane() 
+        {
+            Plane plane = new Plane(Transformation.Translation(new Vec(0.0f,0.0f,-0.2f)));
+            Ray intRay = new Ray(origin: new Point(0.5f, 0.5f, 2.5f), dir: -Constant.VEC_Z);
+            Ray notIntRay = new Ray(origin: new Point(0.0f, 0.0f, 2.5f), dir: Constant.VEC_Z);
+
+            HitRecord? YesHit = plane.rayIntersection(intRay);
+            HitRecord? NoHit = plane.rayIntersection(notIntRay);
+
+            Assert.True(YesHit != null, "TestHitPlane failed! - Assert 1");
+            Assert.True(NoHit == null, "TestHitPlane failed! - Assert 2");
+
+            HitRecord trueHit = new HitRecord(new Point(0.5f,0.5f,-0.2f), new Normal(0.0f,0.0f,1.0f), new Vec2D(0.5f,0.5f), 2.7f, intRay);
+
+            Assert.True(trueHit.isClose(YesHit), "TestHitPlane failed! - Assert 3");
+
+            //  new HitRecord(new Point(), new Normal(0f,0f,1.0f), new Vec2D(0f,0f), 2.5f, intRay));
+
+        }
+
+
+    }
+
 } // end of namespace
