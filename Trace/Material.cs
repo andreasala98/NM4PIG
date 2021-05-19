8b5cd7ccb5228554103ce614234d581daf2ead96
Andrea Sala <andrea.sala98@gmail.com>
2021-05-19 10:34:08 +0200

    Added BRDF

--- Trace/Material.cs
+++ Trace/Material.cs
@@ -1,2 +1,47 @@
-public abstract class BRDF { }
 
+using System;
+
+namespace Trace
+{
+
+    public abstract class Pigment {
+
+        public Color color;
+        public Color getColor(Vec2D uv) {
+            return new Color(0f, 0f, 0f);
+        }
+
+
+    }
+
+
+/// <summary>
+/// An abstract class representing a Bidirectional Reflectance Distribution Function.
+/// Use only derivate classes in the program.
+/// </summary>
+
+    public abstract class BRDF
+    {
+
+        public Pigment pigment;
+        public float reflectance;
+        public BRDF() { }
+        public abstract Color Eval(Normal normal, Vec inDir, Vec outDir, Vec2D uv);
+
+    }
+
+    public class DiffuseBRDF : BRDF {
+
+
+
+        public override Color Eval(Normal normal, Vec inDir, Vec outDir, Vec2D uv)
+        {
+            return this.pigment.getColor(uv) * (this.reflectance / Constant.PI);
+        }
+
+    }
+
+
+
+
+} // Trace
