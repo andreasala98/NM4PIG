1d771ae3f0de921e615432ebc35ea0a9de96b0bb
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-05-19 10:48:07 +0200

    Pigment abs. class and UniformPigment

--- Trace/Material.cs
+++ Trace/Material.cs
@@ -1,2 +1,52 @@
-public abstract class BRDF { }
+/*
+The MIT License (MIT)
 
+Copyright © 2021 Tommaso Armadillo, Pietro Klausner, Andrea Sala
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the “Software”), to deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+the Software. THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+*/
+
+using System;
+
+namespace Trace
+{
+    public abstract class Pigment
+    {
+        // Fields
+        public Color c;
+
+        // methods
+
+        public Pigment(Color col1)
+        {
+            this.c = col1;
+        }
+
+        public Color GetColor(Vec2d v){}
+    }
+
+    public class UniformPigment : Pigment
+    {
+        public UniformPigment(Color? color = null) : base(color) { }
+
+        public override Color GetColor(Vec2d vec)
+        {
+            return this.c;
+        }
+    }
+
+    // public class CheckeredPigment : Pigment
+    // {
+    //     public Color c2;
+    // }
+}
