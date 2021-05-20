8f71b020abb3afd2095405beb023de391dfb8254
Pietro Klausner <pietro.klausner@studenti.unimi.it>
2021-05-20 16:37:46 +0200

    Bilinear interpolation implemented for ImagePigment

--- Trace.Test/MaterialTest.cs
+++ Trace.Test/MaterialTest.cs
@@ -63,10 +63,6 @@ namespace Trace.Test
             image.setPixel(1, 1, new Color(3.0f, 2.0f, 1.0f));
 
             ImagePigment pig = new ImagePigment(image);
-            Vec2D v = new Vec2D(1f, 1f);
-            Console.WriteLine("Rosso = " + pig.getColor(v).r);
-            Console.WriteLine("Verde = " + pig.getColor(v).g);
-            Console.WriteLine("Blu = " + pig.getColor(v).b);
 
             Assert.True(pig.getColor(new Vec2D(0f, 0f)).isClose(new Color(1.0f, 2.0f, 3.0f)), "Test failed, 1/4");
             Assert.True(pig.getColor(new Vec2D(0f, 1f)).isClose(new Color(2.0f, 1.0f, 3.0f)), "Test failed, 2/4");
